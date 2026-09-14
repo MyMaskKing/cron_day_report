6bdfab622fbe214e8d2de851758173bd24799d63
MaskKing <mymask139@163.com>
2026-09-14 14:41:01 +0800

    UI改版

--- android/app/src/main/java/xyz/a10023456/todowidget/MainActivity.kt
+++ android/app/src/main/java/xyz/a10023456/todowidget/MainActivity.kt
@@ -27,6 +27,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.ui.Alignment
