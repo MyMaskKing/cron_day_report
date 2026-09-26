4f3f4bb1238be9a0d8eb8d6fa43a64e8174dcc9e
MaskKing <mymask139@163.com>
2026-09-26 19:26:49 +0800

    修改APP图标

--- android/app/src/main/java/xyz/a10023456/todowidget/ReminderNotifier.kt
+++ android/app/src/main/java/xyz/a10023456/todowidget/ReminderNotifier.kt
@@ -248,7 +248,7 @@ object ReminderNotifier {
         )
         return NotificationCompat.Builder(context, CHANNEL_ID)
             .setSmallIcon(R.drawable.ic_notification)
-            .setColor(0xFFC026D3)
+            .setColor(0xFFC026D3.toInt())
             .setContentTitle(title)
             .setContentText(text)
             .setStyle(NotificationCompat.BigTextStyle().bigText(text))
